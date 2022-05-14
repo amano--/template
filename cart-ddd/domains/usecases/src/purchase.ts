adccefe5382ea146fe4da82f3e54a434e76ad2a2
amano-- <amapetas@gmail.com>
2022-05-14 17:45:02 +0900

    1

--- cart-ddd/domains/usecases/src/purchase.ts
+++ cart-ddd/domains/usecases/src/purchase.ts
@@ -1,7 +1,9 @@
+import { ListProductsInput } from '@me/purchase'
+
 import { getLogger } from 'log4js'
 const logger = getLogger('usecases/purchase')
 
-export const addToCart = async (e: ItemSelectEvent): Promise<AdToPurchaseNaviEvent> => {
+export const listProducts = async (input: ListProductsInput): Promise<AdToPurchaseNaviEvent> => {
   logger.info('event :', e)
   const log = await adApi.saveEvent(e)
   logger.info('save log=', log)
@@ -12,3 +14,15 @@ export const addToCart = async (e: ItemSelectEvent): Promise<AdToPurchaseNaviEve
     naviToPurchaseUrl: '/purchase',
   })
 }
+
+// export const addToCart = async (e: ItemSelectEvent): Promise<AdToPurchaseNaviEvent> => {
+//   logger.info('event :', e)
+//   const log = await adApi.saveEvent(e)
+//   logger.info('save log=', log)
+
+//   return Promise.resolve({
+//     eventType: 'AdToPurchaseNavi',
+//     fromType: e.fromType,
+//     naviToPurchaseUrl: '/purchase',
+//   })
+// }
