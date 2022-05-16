8757d87b82397ed31d8b83528fccc5a023699cfd
amano-- <amapetas@gmail.com>
2022-05-16 15:24:20 +0900

    a

--- cart-ddd/domains/usecases/src/saga/buyProduct.ts
+++ cart-ddd/domains/usecases/src/saga/buyProduct.ts
@@ -3,9 +3,33 @@ import { listRecommendProducts } from '../purchase'
 
 const buyProduct = [selectAd, listRecommendProducts]
 
+const buyProduct2 = { selectAd, listRecommendProducts }
+
 type A = Partial<B>
 // type PickInOut<L> = [][I in L] ? (I extends (a: infer A) => infer R ? [A, R] : never) : never
 
-type PickInOut<L> = L extends Array<infer I> ? (I extends (a: infer A) => Promise<infer R> ? [A, R] : never) : never
+type PickUsecaseInOut<L> = L extends Array<infer I>
+  ? I extends (a: infer A) => Promise<infer R>
+    ? [A, R]
+    : never
+  : never
 
-type V = PickInOut<typeof buyProduct>
+type V = PickUsecaseInOut<typeof buyProduct>
+
+type c<F> = F extends (a: infer A) => Promise<infer R> ? [A, R] : never
+
+type aa<T> = { [P in keyof T]: T[P] }
+
+type ab = aa<typeof buyProduct>
+type d<A> = { [P in keyof A]: c<A[P]> }
+
+type e = d<ab>
+
+type g = 'a' | 'b'
+
+type f<T> = { [P in keyof T]: T[P] }
+
+type he = aa<V>
+
+type ee = f<aa<V>>
+type ac = d<typeof buyProduct2>
