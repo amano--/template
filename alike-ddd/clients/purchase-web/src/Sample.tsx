b0c9263d9496b483566c3b3b0c8c805df305b27d
amano-- <amapetas@gmail.com>
2022-06-23 19:12:28 +0900

    1

---
+++ alike-ddd/clients/purchase-web/src/Sample.tsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+export function Sample() {
+  return (
+    <div>
+      <h1>Sample</h1>
+      <button className="btn btn-primary">Hello daisyUI</button>
+    </div>
+  )
+}
