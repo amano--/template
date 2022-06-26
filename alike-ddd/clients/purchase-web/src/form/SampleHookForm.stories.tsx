4338e235366401c03a753a03035c179d6b2f712f
amano-- <amapetas@gmail.com>
2022-06-26 11:42:56 +0900

    1

--- alike-ddd/clients/purchase-web/src/form/SampleHookForm.stories.tsx
+++ alike-ddd/clients/purchase-web/src/form/SampleHookForm.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
 import { SampleHookForm } from './SampleHookForm'
+import { within, userEvent } from '@storybook/testing-library'
 
 type TargetType = typeof SampleHookForm
 type RequiredStoryObj = ComponentStoryObj<TargetType> & { args: Parameters<TargetType>[0] }
@@ -11,6 +12,21 @@ export const Normal: RequiredStoryObj = {
     firstName: '太郎',
     lastName: 'サンプル',
     age: 33,
+    age2: 34,
     email: 'sample@sample.com',
+  },
+}
+
+export const AfterSubmit: RequiredStoryObj = {
+  args: {
+    firstName: '',
+    lastName: '',
+    age: -10,
+    age2: -10,
+    email: '',
+  },
+  play: async (ctx) => {
+    const canvas = within(ctx.canvasElement)
+    await userEvent.click(canvas.getByText('送信'))
   },
 }
--- alike-ddd/clients/purchase-web/src/form/SampleHookForm.tsx
+++ alike-ddd/clients/purchase-web/src/form/SampleHookForm.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Input } from 'react-daisyui'
+import { Button, Input } from 'react-daisyui'
 
 import { z } from 'zod'
 
@@ -10,11 +10,21 @@ export const schema = z.object({
   lastName: z.string().max(5),
   email: z.string().min(1, { message: 'Required' }),
   age: z.number().min(10),
+  age2: z
+    .string()
+    // カスタムバリデーション
+    .refine((v) => {
+      return !isNaN(Number(v))
+    }, 'error message')
+    // 値を変換
+    .transform((v) => {
+      return Number(v)
+    }),
 })
 
 export type Schema = z.infer<typeof schema>
 
-export const SampleHookForm: FC<Schema> = () => {
+export const SampleHookForm: FC<Schema> = (props) => {
   const {
     register,
     handleSubmit,
@@ -27,15 +37,19 @@ export const SampleHookForm: FC<Schema> = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      firstName : <Input color="primary" {...register('firstName')} />
+      firstName : <Input color="primary" value={props.firstName} {...register('firstName')} />
       {errors.firstName?.message && <p>{errors.firstName?.message}</p>}
-      lastName :<Input color="primary" {...register('lastName')} />
+      lastName :<Input color="primary" value={props.lastName} {...register('lastName')} />
       {errors.lastName?.message && <p>{errors.lastName?.message}</p>}
-      email : <Input color="primary" {...register('email')} />
+      email : <Input color="primary" value={props.email} {...register('email')} />
       {errors.email?.message && <p>{errors.email?.message}</p>}
-      age :<Input color="primary" type="number" {...register('age')} />
+      age :<Input color="primary" type="number" value={props.age} {...register('age')} />
       {errors.age?.message && <p>{errors.age?.message}</p>}
-      <Input color="primary" type="submit" />
+      age2 :<Input color="primary" type="text" value={props.age2} {...register('age2')} />
+      {errors.age2?.message && <p>{errors.age2?.message}</p>}
+      <Button color="primary" type="submit">
+        送信
+      </Button>
     </form>
   )
 }
