eb2c66f952a79150cdcfa91bd720761d1d3ce69e
amano-- <amapetas@gmail.com>
2022-07-05 15:38:38 +0900

    1

--- alike-ddd/clients/purchase-web/src/form/Form.tsx
+++ alike-ddd/clients/purchase-web/src/form/Form.tsx
@@ -1,7 +1,7 @@
-import { InputTextDef } from "./FormDef";
+import { InputTextDef } from './FormDef'
 
 //TODO label の多言語対応
-export type FormBase = { name: string; label: string }
+export type FormBase = { name: string; label: string; error?: string }
 
 // export type FormDefBase = FormBase & { required: boolean }
 // export type InputTextDef = FormDefBase & { ft: 'text' }
