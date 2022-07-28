d14ff519f984d9fd63e561fb3fb0dd2f497862de
amano-- <amapetas@gmail.com>
2022-07-28 15:16:11 +0900

    1

--- alike-ddd/domains/purchase/.eslintrc.js
+++ alike-ddd/domains/purchase/.eslintrc.js
@@ -1,1 +1,10 @@
-module.exports = require('../../configs/min/eslint-preset')
+// module.exports = require('../../configs/min/eslint-preset')
+module.exports = {
+  ...require('../../configs/min/eslint-preset'),
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-case-declarations': 'off',
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-vars': 'off',
+  },
+}
